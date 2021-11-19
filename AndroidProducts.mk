f9029f5a40447e1dbde5368332a840cff68e46db
Manidweep <manidweep678@gmail.com>
2021-11-19 14:33:17 +0100

    Liber: Adapt from lineage to havoc

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_liber.mk
+    $(LOCAL_DIR)/havoc_liber.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_liber-eng \
-    lineage_liber-user \
-    lineage_liber-userdebug
+    havoc_liber-eng \
+    havoc_liber-user \
+    havoc_liber-userdebug
